bb523c08ebfc98b7682cf2e2aba216aed6ed67a9
Willson0 <logvinovnikita3@gmail.com>
2025-08-11 15:08:49 +0500

    frst

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/payment/webhook_jgkgfhs7430jdfsd", [PaymentController::class, 'webhook']);
+Route::post("/payment/webhook_jgkgfhs7430jdfsd", [PaymentController::class, 'webhook']);
